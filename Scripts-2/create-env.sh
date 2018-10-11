833a0547a145ff93a7406ccf54f564f0fc0c3861
Jacob <jkrupa46@gmail.com>
2018-10-11 11:14:10 -0500

    Deleted semicolons

--- Scripts-2/create-env.sh
+++ Scripts-2/create-env.sh
@@ -2,25 +2,25 @@
 
 #script to create environment
 
-apt-get -y update ;
+apt-get -y update
 
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - ;
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - ;
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list ;
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-apt-get -y update ;
-apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn ;
+apt-get -y update 
+apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
 
-apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev ;
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB ;
-curl -sSL https://get.rvm.io | bash -s stable ;
-source /usr/local/rvm/scripts/rvm ;
-rvm install 2.5.1 ;
-rvm use 2.5.1 --default ;
-ruby -v ;
+apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash -s stable
+source /usr/local/rvm/scripts/rvm
+rvm install 2.5.1
+rvm use 2.5.1 --default
+ruby -v
 
-gem install bundler ;
-gem install bundler jekyll ;
-jekyll new site ;
-cd site ;
-bundle exec jekyll serve --host 0.0.0.0 ;
+gem install bundler
+gem install bundler jekyll
+jekyll new site
+cd site
+bundle exec jekyll serve --host 0.0.0.0
